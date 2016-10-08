342b615be899ac92fecc768469e54e04f6debafd
koldovsky <koldovsky@yahoo.com>
2016-10-08 10:39:44 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,13 @@
+loaded.innerHTML = (new Date()).toTimeString();
+
+makeajax.addEventListener('click', function(e) {
+   e.preventDefault();
+   var xhr = new XMLHttpRequest();
+   xhr.onreadystatechange = function() {
+       if (xhr.readyState === 4 && xhr.status === 200 ) {
+          ajaxresult.innerHTML = xhr.responseText;
+       }
+   }
+   xhr.open('GET', 'data.html', true);
+   xhr.send();
+});
