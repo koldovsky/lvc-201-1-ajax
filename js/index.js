a6176beb8fc796ba460d2d60526ba08fa41e8d03
koldovsky <koldovsky@yahoo.com>
2016-10-08 10:48:11 +0000

    jQuery Added

--- js/index.js
+++ js/index.js
@@ -1,13 +1,19 @@
-loaded.innerHTML = (new Date()).toTimeString();
-
-makeajax.addEventListener('click', function(e) {
-   e.preventDefault();
-   var xhr = new XMLHttpRequest();
-   xhr.onreadystatechange = function() {
-       if (xhr.readyState === 4 && xhr.status === 200 ) {
-          ajaxresult.innerHTML = xhr.responseText;
-       }
-   }
-   xhr.open('GET', 'data.html', true);
-   xhr.send();
+$(function () {
+    $('#loaded').html((new Date()).toTimeString());
+    $('#makeajax').click(function()  {
+       $('#ajaxresult').load('data.html');
+    });
+    
+    // makeajax.addEventListener('click', function(e) {
+    //   e.preventDefault();
+    //   var xhr = new XMLHttpRequest();
+    //   xhr.onreadystatechange = function() {
+    //       if (xhr.readyState === 4 && xhr.status === 200 ) {
+    //           ajaxresult.innerHTML = xhr.responseText;
+    //       }
+    //   }
+    //   xhr.open('GET', 'data.html', true);
+    //   xhr.send();
+    // });
+    
 });
